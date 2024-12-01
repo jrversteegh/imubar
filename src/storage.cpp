ebc8dee12e8e69f8cdf0af10a9221c1a39c2e7e1
Jaap Versteegh <j.r.versteegh@gmail.com>
2024-12-01 19:00:28 +0100

    Add flash storage

---
+++ src/storage.cpp
@@ -0,0 +1,25 @@
+#include "storage.h"
+#include "errors.h"
+
+#include <zephyr/logging/log.h>
+
+LOG_MODULE_DECLARE(imubar);
+
+#define SPI_FLASH DT_NODELABEL(spi_flash)
+
+static device const *const spi_flash = DEVICE_DT_GET(SPI_FLASH);
+
+void initialize_storage() {
+  if (!device_is_ready(spi_flash)) {
+    error(2, "SPI FLASH not ready.");
+  }
+  auto page_count = flash_get_page_count(spi_flash);
+  LOG_DBG("Flash has %d pages", page_count);
+  flash_pages_info info;
+  flash_get_page_info_by_offs(spi_flash, 0x0, &info);
+  LOG_DBG("First page at %ld with index %d and size %d", info.start_offset,
+          info.index, info.size);
+  flash_get_page_info_by_offs(spi_flash, 0xFFFFFE, &info);
+  LOG_DBG("Last page at %ld with index %d and size %d", info.start_offset,
+          info.index, info.size);
+}
---
+++ src/storage.h
@@ -0,0 +1,11 @@
+#ifndef IMUBAR_STORAGE_H__
+#define IMUBAR_STORAGE_H__
+
+#include <zephyr/device.h>
+#include <zephyr/drivers/flash.h>
+
+#include "types.h"
+
+extern void initialize_storage();
+
+#endif
