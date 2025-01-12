96ae3937ef60c6c52346b2c9a9b503b1585b6e5d
Jaap Versteegh <j.r.versteegh@gmail.com>
2025-01-12 23:13:22 +0100

    Refactor interface code

--- src/interface.cpp
+++ src/interface.cpp
@@ -7,13 +7,21 @@
 
 LOG_MODULE_DECLARE(imubar);
 
+#ifndef INTERFACE_UART
+#define INTERFACE_UART serial0
+#endif
+
 static device const *const interface_uart =
-    DEVICE_DT_GET(DT_NODELABEL(INTERFACE_UART));
+    DEVICE_DT_GET(DT_ALIAS(INTERFACE_UART));
 
 RING_BUF_DECLARE(interface_input, 256);
 RING_BUF_DECLARE(interface_output, 256);
 
-static UartData uart_data{&interface_input, &interface_output, "INTERFACE_UART"};
+static UartData uart_data{&interface_input, &interface_output,
+                          "INTERFACE_UART"};
+
+static constexpr char const *const ping = "INTFPING\r\n";
+static constexpr char const *const pong = "INTFPONG\r\n";
 
 int interface_write(uint8_t const *data, size_t size) {
   return serial_write(interface_uart, data, size, &uart_data);
@@ -23,10 +31,15 @@ int interface_read(uint8_t *data, size_t size) {
   return serial_read(interface_uart, data, size, &uart_data);
 }
 
-void initialize_interface() {
+void interface_ping() {
+  static int const ping_len = strlen(ping);
+  interface_write((uint8_t *)ping, ping_len);
+}
+
+void interface_init() {
   if (!serial_init(interface_uart, &uart_data)) {
     error(2, "Interface UART not ready.");
   }
 
-  serial_write(interface_uart, (uint8_t*)"\r\nINTFINIT\r\n", 12, &uart_data);
+  serial_write(interface_uart, (uint8_t *)"\r\nINTFINIT\r\n", 12, &uart_data);
 }
--- src/interface.h
+++ src/interface.h
@@ -3,8 +3,9 @@
 
 #include "types.h"
 
-extern void initialize_interface();
+extern void interface_init();
 extern int interface_read(uint8_t *data, size_t size);
 extern int interface_write(uint8_t const *data, size_t size);
+extern void interface_ping();
 
 #endif
